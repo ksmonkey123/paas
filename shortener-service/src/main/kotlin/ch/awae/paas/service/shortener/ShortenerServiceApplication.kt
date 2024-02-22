8bcff34d6d6d60d1d64bcdb7aa35bc468d6f474f
Andreas Wälchli <andreas.waelchli@me.com>
2024-02-22 23:35:25 +0000

    create empty shortener service

---
+++ shortener-service/src/main/kotlin/ch/awae/paas/service/shortener/ShortenerServiceApplication.kt
@@ -0,0 +1,14 @@
+package ch.awae.paas.service.shortener
+
+import ch.awae.paas.auth.*
+import org.springframework.boot.*
+import org.springframework.boot.autoconfigure.*
+import org.springframework.context.annotation.*
+
+@SpringBootApplication
+@Import(ClientSecurityConfiguration::class)
+class ShortenerServiceApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ShortenerServiceApplication::class.java, *args)
+}
